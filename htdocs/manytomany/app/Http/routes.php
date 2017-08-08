fba57230a706c26a27f293adbc5fab08c383a340
qiuxan <qiuxan@gmail.com>
2017-08-08 23:32:38 +1000

    many to many CRUD practice

---
+++ htdocs/manytomany/app/Http/routes.php
@@ -0,0 +1,97 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+use App\Role;
+use App\User;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/create',function (){
+
+    $user =User::findOrFail(1);
+
+//    $role=new Role(['name'=>'Administrator']);
+
+    $user->roles()->save(new Role(['name'=>'Subscriber']));
+
+});
+
+Route::get('/read',function (){
+
+    $user= User::findOrFail(1);
+
+    foreach ( $user->roles as $role){
+
+        echo $role->namev ;
+    }
+
+});
+
+Route::get('/update',function (){
+
+    $user=User::findOrFail(1);
+
+    if ($user->has('roles')){
+
+        foreach ($user->roles as $role){
+
+            if($role->name=='Administrator'){
+
+                $role->name="subscriber";
+
+                $role->save();
+            }
+
+        }
+    }
+
+});
+
+
+Route::get('/delete',function (){
+
+   $user =User::findOrFail(1);
+//   $user->roles()->delete();//delete every thing ni  the table
+
+
+    foreach ($user->roles as $role){
+
+    $role->whereId(3)->delete();
+    }
+});
+
+
+Route::get('/attach',function (){
+
+    $user=User::findOrFail(1);
+    $user->roles()->attach(4);
+
+});
+
+Route::get('/detach',function (){
+
+    $user=User::findOrFail(1);
+    $user->roles()->detach(4);
+
+});
+
+Route::get('/sync',function (){
+
+    $user=User::findOrFail(1);
+
+    $user->roles()->sync([2,5,6,8]);
+
+});
