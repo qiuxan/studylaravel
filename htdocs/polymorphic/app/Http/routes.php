bdb5e46e13f58fb218f02a855d2c4be3d9391732
qiuxan <qiuxan@gmail.com>
2017-08-09 23:10:45 +1000

    polymorphic db crud practice 2

--- htdocs/polymorphic/app/Http/routes.php
+++ htdocs/polymorphic/app/Http/routes.php
@@ -39,3 +39,44 @@ Route::get('/read',function (){
     }
 
 });
+
+Route::get('/update',function (){
+
+
+    $staff=Staff::findOrFail(1);
+
+    $photo=$staff->photos()->whereId(1)->first();
+
+    $photo->path="updated path.jpg";
+    $photo->save();
+
+});
+
+Route::get('/delete',function (){
+
+    $staff= Staff::findOrFail(1);
+
+    $staff->photos()->whereId(1)->delete();
+
+
+});
+
+
+Route::get('/assign',function (){
+
+   $staff=Staff::findOrFail(1);
+
+   $photo=Photo::findOrFail(2);
+
+   $staff->photos()->save($photo);
+
+});
+
+Route::get('/un-sign',function (){
+
+    $staff=Staff::findOrFail(1);
+
+
+    $staff->photos()->whereId(3)->update(['imageable_id'=>'','imageable_type'=>0]);
+
+});
