fe3c32e0285365833454d695ca5a589c8870d07f
qiuxan <qiuxan@gmail.com>
2017-08-14 16:03:01 +1000

    using package to buid the form -edit and delete

--- htdocs/Laravel/resources/views/posts/create.blade.php
+++ htdocs/Laravel/resources/views/posts/create.blade.php
@@ -23,7 +23,7 @@
     <div class="form-group">
 
 
-    {!! Form::submit('Create Post', ['class'=>'btn btn-primary']) !!}
+        {!! Form::submit('Create Post', ['class'=>'btn btn-primary']) !!}
 
     </div>
 
--- htdocs/Laravel/resources/views/posts/edit.blade.php
+++ htdocs/Laravel/resources/views/posts/edit.blade.php
@@ -4,32 +4,70 @@
 
     <h1>Edit Post</h1>
 
-    <form method="post" action="/laravel/public/posts/{{$post->id}}">
+    {!! Form::model($post, ['method'=>'PATCH', 'action'=>['PostsController@update',$post->id]]) !!}
 
+    {{--<form method="post" action="/laravel/public/posts/{{$post->id}}">--}}
 
-        <input type="hidden" name="_method" value="PUT">
-        <input type="text"  name="title" placeholder="Enter title4" value="{{$post->title}}">
 
 
         {{csrf_field()}}
 
 
+    {{--<input type="hidden" name="_method" value="PUT">--}}
+    {{--<input type="text"  name="title" placeholder="Enter title4" value="{{$post->title}}">--}}
 
-        <input type="submit" name="UPDATE">
 
-    </form>
+    <div class="form-group">
 
-    <form method="post" action="/laravel/public/posts/{{$post->id}}">
+        {!! Form::label('title',"Title: ") !!}
+
+        {!! Form::text('title',null, ['class'=>'form-control']) !!}
+
+    </div>
+
+
+
+        {{--<input type="submit" name="UPDATE">--}}
+        {!! Form::submit('Update Post', ['class'=>'btn btn-info']) !!}
+
+
+    {{--</form>--}}
+    {!! Form::close() !!}
+
+
+
+
+    {!! Form::open( ['method'=>'DELETE', 'action'=>['PostsController@destroy',$post->id]]) !!}
+    {{--<form method="post" action="/laravel/public/posts/{{$post->id}}">--}}
+
 
         {{csrf_field()}}
 
-        <input type="hidden" name="_method" value="DELETE">
+        {{--<input type="hidden" name="_method" value="DELETE">--}}
 
-        <input type="submit" value="DELETE">
+        {{--<input type="submit" value="DELETE">--}}
+
+        {!! Form::submit('Delete Post', ['class'=>'btn btn-danger']) !!}
 
 
 
-    </form>
+
+
+
+    {{--</form>--}}
+
+
+
+    {!! Form::close() !!}
+
+
+
+
+
+
+
+
+
 
 
 @endsection
