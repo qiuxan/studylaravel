3ca32a308944ce213b70a55d9ade9983445a67d3
qiuxan <qiuxan@gmail.com>
2017-08-25 23:37:24 +1000

    lay out build table and using css in laravel

--- htdocs/codehacking/app/Http/Controllers/AdminUsersController.php
+++ htdocs/codehacking/app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -17,7 +18,12 @@ class AdminUsersController extends Controller
     {
         //
 
-        return view('admin.users.index');
+        $users=User::all();
+
+//        return view('admin.users.index');
+
+
+        return view('admin.users.index',compact('users'));
 
 
     }
