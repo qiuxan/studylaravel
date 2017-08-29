d8ed83ece43e168689951496543d5a6dc684e892
qiuxan <qiuxan@gmail.com>
2017-08-29 12:45:28 +1000

    users with photo works

--- htdocs/codehacking/app/Http/Controllers/AdminUsersController.php
+++ htdocs/codehacking/app/Http/Controllers/AdminUsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UsersRequest;
+use App\Photo;
 use App\Role;
 use App\User;
 use Illuminate\Http\Request;
@@ -56,14 +57,26 @@ class AdminUsersController extends Controller
         //
 
 
+        $input=$request->all();
+        if($file=$request->file('photo_id')){
 
 
-        User::create($request->all());
+            $name=time().$file->getClientOriginalName();
+            $file->move('images',$name);
+            $photo=Photo::create(['file'=>$name]);
+            $input['photo_id']=$photo->id;
 
-        return redirect('/admin/users');
+        }
+
+        $input['password']=bcrypt($request->password);
+        User::create($input);
+
+//        User::create($request->all());
+
+//        return redirect('/admin/users');
 
        // return $request->all();
-        
+
 
     }
 
