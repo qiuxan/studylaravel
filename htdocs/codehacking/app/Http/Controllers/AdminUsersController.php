7e76c210867e429c372fc7421808ba39529938f2
qiuxan <qiuxan@gmail.com>
2017-08-31 23:41:21 +1000

    delete feature setup and work

--- htdocs/codehacking/app/Http/Controllers/AdminUsersController.php
+++ htdocs/codehacking/app/Http/Controllers/AdminUsersController.php
@@ -189,5 +189,9 @@ class AdminUsersController extends Controller
     public function destroy($id)
     {
         //
+        User::findOrFail($id)->delete();
+
+
+        return redirect('/admin/users');
     }
 }
