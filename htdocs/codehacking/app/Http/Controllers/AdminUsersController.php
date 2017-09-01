8666c7b4f31b74036bb730cbac9c3cbb83fca01e
qiuxan <qiuxan@gmail.com>
2017-09-01 23:24:29 +1000

    flash message

--- htdocs/codehacking/app/Http/Controllers/AdminUsersController.php
+++ htdocs/codehacking/app/Http/Controllers/AdminUsersController.php
@@ -10,6 +10,7 @@ use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Session;
 
 class AdminUsersController extends Controller
 {
@@ -190,6 +191,8 @@ class AdminUsersController extends Controller
     {
         //
         User::findOrFail($id)->delete();
+
+        Session::flash('deleted_user','The user has been deleted');
 
 
         return redirect('/admin/users');
