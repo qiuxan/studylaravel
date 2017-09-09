d4275a6a1fedd9aced3898b4b0d25d1b7fefc9d5
qiuxan <qiuxan@gmail.com>
2017-09-09 23:59:51 +1000

    edit and delete features

--- htdocs/codehacking/app/Http/Controllers/AdminCategoriesController.php
+++ htdocs/codehacking/app/Http/Controllers/AdminCategoriesController.php
@@ -70,6 +70,10 @@ class AdminCategoriesController extends Controller
     public function edit($id)
     {
         //
+
+        $category =Category::findOrFail($id);
+
+        return view('admin.categories.edit',compact('category'));
     }
 
     /**
@@ -82,6 +86,12 @@ class AdminCategoriesController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $category=  Category::findOrFail($id);
+
+        $category->update($request->all());
+
+        return redirect('admin/categories');
+
     }
 
     /**
@@ -92,6 +102,11 @@ class AdminCategoriesController extends Controller
      */
     public function destroy($id)
     {
+
+        Category::findOrFail($id)->delete();
+
+        return redirect('admin/categories');
+
         //
     }
 }
