0a9a6ac0a88b6d691fa33917f8dae433391c13cf
qiuxan <qiuxan@gmail.com>
2017-10-25 22:31:44 +1100

    fix login feature for laravel 5.3

--- htdocs/codehacking/routes/web.php
+++ htdocs/codehacking/routes/web.php
@@ -11,6 +11,40 @@
 |
 */
 
+Auth::routes();
+
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/logout', 'Auth\LoginController@logout');
+
+
+
+Route::group(['middleware'=>'admin'],function (){
+
+    Route::get('/admin',function (){
+
+
+        return view('admin.index');
+
+
+    });
+
+
+    Route::resource('admin/users','AdminUsersController');
+
+    Route::resource('admin/posts','AdminPostsController');
+
+    Route::resource('admin/categories','AdminCategoriesController');
+
+    Route::resource('admin/media','AdminMediasController');
+
+    Route::resource('admin/comments', 'PostCommentsController');
+
+    Route::resource('admin/comment/replies', 'CommentRepliesController');
+
+
+
 });
