c9d12c25324875650315c4b514addb639b944ab9
qiuxan <qiuxan@gmail.com>
2017-10-29 23:21:02 +1100

    fix some bug

--- htdocs/codehacking/routes/web.php
+++ htdocs/codehacking/routes/web.php
@@ -43,6 +43,9 @@ Route::group(['middleware'=>'admin'],function (){
 
     ]]);
 
+    Route::get('/post/{id}',['as'=>'home.post', 'uses'=>'AdminPostsController@post']);
+
+
     Route::resource('admin/posts','AdminPostsController',['names'=>[
 
         'index'=>'admin.posts.index',
@@ -78,7 +81,8 @@ Route::group(['middleware'=>'admin'],function (){
         'index'=>'admin.comments.index',
         'create'=>'admin.comments.create',
         'store'=>'admin.comments.store',
-        'edit'=>'admin.comments.edit'
+        'edit'=>'admin.comments.edit',
+        'show'=>'admin.comments.show'
 
 
     ]]);
